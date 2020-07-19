7cb80f83baff134a4d9fe32a21b1c49134973292
Simas89 <simaszurauskas@gmail.com>
2020-07-19 08:35:54 +0100

    before moving state to reducer

--- client/src/components/landing/Formike.js
+++ client/src/components/landing/Formike.js
@@ -5,37 +5,121 @@ import { Input, Icon, Button, Form, TextArea } from "semantic-ui-react";
 const Formike = (props) => {
 	const [LR, setLR] = React.useState(1);
 	const [rememberMe, setRememberMe] = React.useState(0);
-	const [textAreaFocused, setTextAreaFocused] = React.useState(false);
+	const [focused, setFocused] = React.useState({
+		userName: false,
+		psw1: false,
+		psw2: false,
+		senderName: false,
+		senderMail: false,
+		textArea: false,
+	});
+	const [userInputs, setUserInputs] = React.useState({
+		userName: "",
+		psw1: "",
+		psw2: "",
+	});
+
+	// SET AND CORRECT INPUTS
+	const minLengthUser = 3;
+	const minLengthPsw = 6;
+	const userInputsChange = (e, type) => {
+		const value = e.target.value.replace(/[^A-Za-z0-9]/gi, "");
+
+		if (value.length <= 18) setUserInputs({ ...userInputs, [type]: value });
+	};
+
 	return (
 		<div className='form-wrapper'>
 			{props.type === "LOGIN" && (
 				<React.Fragment>
 					<h1>{LR ? "Create new account" : "Log in"}</h1>
 					<div className='inputs-wrapper'>
-						<Input
-							icon={<Icon name='user' size='large' />}
-							iconPosition='left'
-							className='txt-input'
-							type='text'
-							spellCheck='false'
-							transparent></Input>
-						<div style={{ borderBottom: "1px solid green" }}>
+						{/*-------------          USER NAME        ---------------  */}
+						<div className='single-input-wrapper'>
+							<div className='single-input-wrapper-subIcon'>
+								<Icon
+									style={
+										userInputs.userName.length >= minLengthUser
+											? { display: "block" }
+											: { display: "none" }
+									}
+									className={focused.userName ? "icon-bright" : "icon-fade"}
+									name={"check"}
+									size='small'
+								/>
+							</div>
 							<Input
-								icon={<Icon name='key' size='large' />}
+								onChange={(e) => userInputsChange(e, "userName")}
+								value={userInputs.userName}
+								onFocus={() => setFocused({ ...focused, userName: true })}
+								onBlur={() => setFocused({ ...focused, userName: false })}
+								icon={<Icon name='user' size='large' />}
+								iconPosition='left'
+								className='txt-input'
+								type='text'
+								spellCheck='false'
+								transparent></Input>
+						</div>
+						{/*-------------          PASSWORD 1       ---------------  */}
+						<div className='single-input-wrapper'>
+							<div className='single-input-wrapper-subIcon'>
+								<Icon
+									style={
+										userInputs.psw1.length >= minLengthPsw
+											? { display: "block" }
+											: { display: "none" }
+									}
+									className={focused.psw1 ? "icon-bright" : "icon-fade"}
+									name={
+										userInputs.psw1.length >= minLengthPsw ? "check" : "delete"
+									}
+									size='small'
+								/>
+							</div>
+							<Input
+								onChange={(e) => userInputsChange(e, "psw1")}
+								value={userInputs.psw1}
+								onFocus={() => setFocused({ ...focused, psw1: true })}
+								onBlur={() => setFocused({ ...focused, psw1: false })}
+								icon={<Icon className='icon-input' name='key' size='large' />}
 								iconPosition='left'
 								className='txt-input'
 								type='password'
 								spellCheck='false'
 								transparent></Input>
 						</div>
+						{/*-------------     PASSWORD 2  ---------------  */}
 						{LR ? (
-							<Input
-								icon={<Icon name='key' size='large' />}
-								iconPosition='left'
-								className='txt-input'
-								type='password'
-								spellCheck='false'
-								transparent></Input>
+							<div className='single-input-wrapper'>
+								<div className='single-input-wrapper-subIcon'>
+									<Icon
+										style={
+											userInputs.psw2.length >= minLengthPsw
+												? { display: "block" }
+												: { display: "none" }
+										}
+										className={focused.psw2 ? "icon-bright" : "icon-fade"}
+										name={
+											userInputs.psw2.length >= minLengthPsw &&
+											userInputs.psw2 === userInputs.psw1
+												? "check"
+												: "delete"
+										}
+										size='small'
+									/>
+								</div>
+								<Input
+									onChange={(e) => userInputsChange(e, "psw2")}
+									value={userInputs.psw2}
+									onFocus={() => setFocused({ ...focused, psw2: true })}
+									onBlur={() => setFocused({ ...focused, psw2: false })}
+									icon={<Icon name='key' size='large' />}
+									iconPosition='left'
+									className='txt-input'
+									type='password'
+									spellCheck='false'
+									transparent></Input>
+							</div>
 						) : (
 							""
 						)}
@@ -60,7 +144,7 @@ const Formike = (props) => {
 							</Button>
 						</Form>
 						<div className='log-reg-switch'>
-							{LR ? "Already a user? " : "Dont have an account? "}
+							{LR ? "Already a user? " : "Don't have an account? "}
 							<span onClick={() => setLR(!LR)}>
 								{LR ? " Log in" : " Register"}
 							</span>
@@ -73,6 +157,8 @@ const Formike = (props) => {
 					<h1>Contact Me</h1>
 					<div className='inputs-wrapper'>
 						<Input
+							onFocus={() => setFocused({ ...focused, senderName: true })}
+							onBlur={() => setFocused({ ...focused, senderName: false })}
 							icon={<Icon name='user' size='large' />}
 							iconPosition='left'
 							className='txt-input'
@@ -80,6 +166,8 @@ const Formike = (props) => {
 							spellCheck='false'
 							transparent></Input>
 						<Input
+							onFocus={() => setFocused({ ...focused, senderMail: true })}
+							onBlur={() => setFocused({ ...focused, senderMail: false })}
 							icon={<Icon name='mail outline' size='large' />}
 							iconPosition='left'
 							className='txt-input'
@@ -88,14 +176,14 @@ const Formike = (props) => {
 							transparent></Input>
 						<Form className='forma'>
 							<Icon
-								className={textAreaFocused ? "iconas-white" : "iconas-grey"}
+								className={focused.textArea ? "iconas-white" : "iconas-grey"}
 								name='pencil'
 								size='large'
 								color='grey'
 							/>
 							<TextArea
-								onFocus={() => setTextAreaFocused(true)}
-								onBlur={() => setTextAreaFocused(false)}
+								onFocus={() => setFocused({ ...focused, textArea: true })}
+								onBlur={() => setFocused({ ...focused, textArea: false })}
 								className='text-area'
 								spellCheck='false'
 							/>
