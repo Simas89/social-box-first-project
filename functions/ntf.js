69055fd7afa67a091ab0505f276c451a792af833
Simas89 <simaszurauskas@gmail.com>
2020-08-25 09:21:13 +0100

    ...

--- functions/ntf.js
+++ functions/ntf.js
@@ -1,3 +1,7 @@
+const UserModel = require("../schemas/userSchema");
+const NotificationsList = require("../schemas/notificationsMODEL");
+const profileImgSmall = require("../schemas/profileImgSmall");
+
 const ntf = () => {
 	console.log("NTF");
 };
